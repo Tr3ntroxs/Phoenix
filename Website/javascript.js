4662ba94263466e0cea265cc5b44da06aacc2153
Tr3ntroxs <113727648+Tr3ntroxs@users.noreply.github.com>
2023-03-17 18:55:32 -0400

    Website Main Page

--- Website/javascript.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-  const loader = document.querySelector(".loader");
-
-  loader.classList.add("loader--hidden");
-
-  loader.addEventListener("transitionend", () => {
-    document.body.removeChild(loader);
-  });
-});
