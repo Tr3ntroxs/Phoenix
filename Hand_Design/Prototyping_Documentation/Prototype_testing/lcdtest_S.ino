26b69d2320425407957e35f7266ae07e80038e22
SpadeZap <72225926+SpadeZap@users.noreply.github.com>
2023-06-23 15:20:16 -0400

    Added Servo and LED sync test
    Servo and LED go off at the same time.

--- Hand_Design/Prototyping_Documentation/Prototype_testing/lcdtest_S.ino
+++ Hand_Design/Prototyping_Documentation/Prototype_testing/lcdtest_S.ino
@@ -4,10 +4,17 @@ LiquidCrystal lcd(12, 11, 7, 6, 5, 4);
 
 // Pin locations: 
 // GND - 5V, negative ground (parallel to it)
-// VCC - GND positive ground
+// VCC - GND positive ground (parallel to it)
 // Vo - Pin 13
 // RS - Pin 12
 // R/W - Neg Ground
+// Enable - 11
+// D4 - 7
+// D5 - 6
+// D6 - 5
+// D7 - 4
+// A - Pos Ground
+// K - Neg Ground
 void setup()
 {
   lcd.begin(16, 2);
