b98ac334a1d2e3742776e37b604da3d32d1e298a
EmilioBejasa <69165764+EmilioBejasa@users.noreply.github.com>
2023-08-04 14:12:34 -0400

    Moved the footer to the bottom
    This will prob be moved bck up in the future due to the slideshow not being implemented yet.

--- Website/phoenix-app/src/components/Footer.js
+++ Website/phoenix-app/src/components/Footer.js
@@ -29,12 +29,14 @@ const Footer = () => {
         <div className="footer-item">
           <h3>Socials</h3>
           <div className="footer-col">
-            <a href="https://github.com/Tr3ntroxs/HandiCraft">
-              <button type="button">Github</button>
-            </a>
-            <a href="https://cad.onshape.com/documents/96d875bf46dff56dd5444c32/w/6ed33167ab82f3a660f521c9/e/e5734e374defbcc0d33a5eee">
-              <button type="button">Onshape</button>
-            </a>
+            <ul>
+            <li><a href="https://github.com/Tr3ntroxs/HandiCraft">
+              Github
+            </a></li>
+            <li><a href="https://cad.onshape.com/documents/96d875bf46dff56dd5444c32/w/6ed33167ab82f3a660f521c9/e/e5734e374defbcc0d33a5eee">
+              Onshape
+            </a></li>
+            </ul>
           </div>
         </div>
       </div>
