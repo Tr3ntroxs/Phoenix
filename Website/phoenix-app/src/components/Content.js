a0f2231cdfc0d3954d838316e4023c4d913073d4
EmilioBejasa <69165764+EmilioBejasa@users.noreply.github.com>
2023-08-04 14:54:56 -0400

    SLIDER IMPLEMENTED
    Got the slideshow working!!!

--- Website/phoenix-app/src/components/Content.js
+++ Website/phoenix-app/src/components/Content.js
@@ -3,30 +3,23 @@ import '../Content.css';
 import arduino from '../arduino.jpg'
 import cad from '../CAD_example.png'
 import sensor from '../sensor.jpg'
+import ImageSlider from './Slider';
 
 const Content = () => {
+  const SliderData = [
+    {image: arduino, caption: "Powered by Arduino"}, 
+    {image: cad, caption: "How OnShape was Utilized"}, 
+    {image: sensor, caption: "How does a Piezoelectric Sensor work?"},
+  ];
   return (
     <div className="content">
       <div>
         <h1>Second Chances</h1>
         <p>Powered by Arduino ELEGOO and designed through Onshape, <br /> Phoenix will lend more than just a hand...</p>
       </div>
-    
-      <div className="slider-frame">
-        <div class="slide-images">
-          <div class="img-container s1">
-            <h2>Powered by Arduino</h2>
-            <img src={arduino} alt={"arduino"} />
-          </div>
-          <div class="img-container s2">
-            <h2>How OnShape was Utilized</h2>
-            <img src={cad} alt={"CAD"} />
-          </div>
-          <div class="img-container s3">
-            <h2>How does a Piezoelectric Sensor work?</h2>
-            <img src={sensor} alt={"sensor"} />
-          </div>
-        </div>
+
+      <div>
+        <ImageSlider slides={SliderData}/>
       </div>
     </div>
   );
---
+++ Website/phoenix-app/src/components/Slider.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const ImageSlider = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
+  return (
+    <section className='slider'>
+      <button className='prev-slide' onClick={prevSlide}>Previous</button>
+      <button className='next-slide' onClick={nextSlide}>Next</button>
+      {slides.map((slide, index) => {
+        return (
+          <div
+            className={index === current ? 'slide active' : 'slide'}
+            key={index}
+          >
+            {index === current && (
+              <h2>{slide.caption}</h2>
+            )}
+            {index === current && (
+              <img src={slide.image} alt='travel image' className='image' />
+            )}
+          </div>
+        );
+      })}
+    </section>
+  );
+};
+
+export default ImageSlider;
